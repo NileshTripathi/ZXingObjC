d2b204dffeedbbda0f7580faae4f28e90dda2a19
Costa Walcott <cwalcott@gmail.com>
2014-03-21 14:25:08 -0400

    Add ZX prefix to error instance helper methods

--- ZXingObjC/oned/ZXUPCEANReader.m
+++ ZXingObjC/oned/ZXUPCEANReader.m
@@ -170,18 +170,18 @@ const int ZX_UPC_EAN_L_AND_G_PATTERNS[ZX_UPC_EAN_L_AND_G_PATTERNS_LEN][ZX_UPC_EA
   int end = (int)NSMaxRange(endRange);
   int quietEnd = end + (end - (int)endRange.location);
   if (quietEnd >= [row size] || ![row isRange:end end:quietEnd value:NO]) {
-    if (error) *error = NotFoundErrorInstance();
+    if (error) *error = ZXNotFoundErrorInstance();
     return nil;
   }
 
   NSString *resultString = [result description];
   // UPC/EAN should never be less than 8 chars anyway
   if ([resultString length] < 8) {
-    if (error) *error = FormatErrorInstance();
+    if (error) *error = ZXFormatErrorInstance();
     return nil;
   }
   if (![self checkChecksum:resultString error:error]) {
-    if (error) *error = ChecksumErrorInstance();
+    if (error) *error = ZXChecksumErrorInstance();
     return nil;
   }
 
@@ -214,7 +214,7 @@ const int ZX_UPC_EAN_L_AND_G_PATTERNS[ZX_UPC_EAN_L_AND_G_PATTERNS_LEN][ZX_UPC_EA
   if ([[self class] checkStandardUPCEANChecksum:s]) {
     return YES;
   } else {
-    if (error) *error = FormatErrorInstance();
+    if (error) *error = ZXFormatErrorInstance();
     return NO;
   }
 }
@@ -294,7 +294,7 @@ const int ZX_UPC_EAN_L_AND_G_PATTERNS[ZX_UPC_EAN_L_AND_G_PATTERNS_LEN][ZX_UPC_EA
     }
   }
 
-  if (error) *error = NotFoundErrorInstance();
+  if (error) *error = ZXNotFoundErrorInstance();
   return NSMakeRange(NSNotFound, 0);
 }
 
@@ -303,7 +303,7 @@ const int ZX_UPC_EAN_L_AND_G_PATTERNS[ZX_UPC_EAN_L_AND_G_PATTERNS_LEN][ZX_UPC_EA
  */
 + (int)decodeDigit:(ZXBitArray *)row counters:(ZXIntArray *)counters rowOffset:(int)rowOffset patternType:(ZX_UPC_EAN_PATTERNS)patternType error:(NSError **)error {
   if (![self recordPattern:row start:rowOffset counters:counters]) {
-    if (error) *error = NotFoundErrorInstance();
+    if (error) *error = ZXNotFoundErrorInstance();
     return -1;
   }
   int bestVariance = ZX_UPC_EAN_MAX_AVG_VARIANCE;
@@ -347,7 +347,7 @@ const int ZX_UPC_EAN_L_AND_G_PATTERNS[ZX_UPC_EAN_L_AND_G_PATTERNS_LEN][ZX_UPC_EA
   if (bestMatch >= 0) {
     return bestMatch;
   } else {
-    if (error) *error = NotFoundErrorInstance();
+    if (error) *error = ZXNotFoundErrorInstance();
     return -1;
   }
 }
